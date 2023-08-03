2e9c57190362efdff6e276daf6ae5a2e50c693b6
deniskeller <dentean@mail.ru>
2023-08-03 12:06:35 +0400

    ref2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,29 +14,6 @@ export default function Document() {
 					rel='stylesheet'
 					href='https://use.typekit.net/fzy6ewm.css'
 				></link>
-
-				{/* <link
-					rel='apple-touch-icon'
-					sizes='180x180'
-					href='/apple-touch-icon.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='32x32'
-					href='/favicon-32x32.png'
-				/>
-				<link
-					rel='icon'
-					type='image/png'
-					sizes='16x16'
-					href='/favicon-16x16.png'
-				/>
-				<link rel='manifest' href='/site.webmanifest' />
-				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5f41d4' />
-				<meta name='msapplication-TileColor' content='#5f41d4' />
-				<meta name='theme-color' content='#5f41d4'></meta>
-				<link rel='icon' type='image/png' href='/favicon.ico'></link> */}
 			</Head>
 			<title>Cloud ladz</title>
 			<body>
